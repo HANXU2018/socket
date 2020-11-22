3ebe3807f848e47aa8f5fb8ab2c51f20ba9eee7f
HANXU2018 <1076998404@qq.com>
2020-11-22 22:03:23 +0800

    Update TCPServerThread.java

--- src/cn/hanxu51/socket/TCPServerThread.java
+++ src/cn/hanxu51/socket/TCPServerThread.java
@@ -24,11 +24,13 @@ public class TCPServerThread {
         while(true){
             //使用accept方法获取到请求的客户端对象(浏览器)
             Socket socket = server.accept();
+            System.out.println(socket.getInetAddress().getHostAddress());
             new Thread(new Runnable() {
                 @Override
                 public void run() {
                     try {
                         //使用Socket对象中的方法getInputStream,获取到网络字节输入流InputStream对象
+                        System.out.println(socket.getLocalAddress());
                         InputStream is = socket.getInputStream();
                         //使用网络字节输入流InputStream对象中的方法read读取客户端的请求信息
                         /*byte[] bytes = new byte[1024];
